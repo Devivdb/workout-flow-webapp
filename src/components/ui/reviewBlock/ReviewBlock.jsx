eabb957790338052bbc8824b670f9a2b99925b8e
Devivdb <devivdb@gmail.com>
2025-02-10 14:53:57 +0100

    Add base structure and CSS styling for the testimonials page

--- src/components/ui/reviewBlock/ReviewBlock.jsx
+++ src/components/ui/reviewBlock/ReviewBlock.jsx
@@ -1,12 +1,92 @@
 import './ReviewBlock.css'
 
 function ReviewBlock() {
-
     return (
         <>
-
+            <div className="outer-container-wrapper-review">
+                <div className="review">
+                    <p>⭐⭐⭐⭐⭐</p>
+                    <p>
+                        I've been using this workout builder for a few weeks now, and I love how customizable it is! I
+                        can
+                        adjust sets, reps, rest time, and even add my own exercises. It’s perfect for tailoring my
+                        workouts
+                        to my fitness goals.
+                    </p>
+                </div>
+                <div className="review">
+                    <p>⭐⭐⭐⭐⭐</p>
+                    <p>
+                        I've been using this workout builder for a few weeks now, and I love how customizable it is! I
+                        can
+                        adjust sets, reps, rest time, and even add my own exercises. It’s perfect for tailoring my
+                        workouts
+                        to my fitness goals.
+                    </p>
+                </div>
+                <div className="review">
+                    <p>⭐⭐⭐⭐⭐</p>
+                    <p>
+                        I've been using this workout builder for a few weeks now, and I love how customizable it is! I
+                        can
+                        adjust sets, reps, rest time, and even add my own exercises. It’s perfect for tailoring my
+                        workouts
+                        to my fitness goals.
+                    </p>
+                </div>
+                <div className="review">
+                    <p>⭐⭐⭐⭐⭐</p>
+                    <p>
+                        I've been using this workout builder for a few weeks now, and I love how customizable it is! I
+                        can
+                        adjust sets, reps, rest time, and even add my own exercises. It’s perfect for tailoring my
+                        workouts
+                        to my fitness goals.
+                    </p>
+                </div>
+                <div className="review">
+                    <p>⭐⭐⭐⭐⭐</p>
+                    <p>
+                        I've been using this workout builder for a few weeks now, and I love how customizable it is! I
+                        can
+                        adjust sets, reps, rest time, and even add my own exercises. It’s perfect for tailoring my
+                        workouts
+                        to my fitness goals.
+                    </p>
+                </div>
+                <div className="review">
+                    <p>⭐⭐⭐⭐⭐</p>
+                    <p>
+                        I've been using this workout builder for a few weeks now, and I love how customizable it is! I
+                        can
+                        adjust sets, reps, rest time, and even add my own exercises. It’s perfect for tailoring my
+                        workouts
+                        to my fitness goals.
+                    </p>
+                </div>
+                <div className="review">
+                    <p>⭐⭐⭐⭐⭐</p>
+                    <p>
+                        I've been using this workout builder for a few weeks now, and I love how customizable it is! I
+                        can
+                        adjust sets, reps, rest time, and even add my own exercises. It’s perfect for tailoring my
+                        workouts
+                        to my fitness goals.
+                    </p>
+                </div>
+                <div className="review">
+                    <p>⭐⭐⭐⭐⭐</p>
+                    <p>
+                        I've been using this workout builder for a few weeks now, and I love how customizable it is! I
+                        can
+                        adjust sets, reps, rest time, and even add my own exercises. It’s perfect for tailoring my
+                        workouts
+                        to my fitness goals.
+                    </p>
+                </div>
+            </div>
         </>
     )
 }
 
-export default ReviewBlock
+export default ReviewBlock;
--- src/pages/testimonials/Testimonials.jsx
+++ src/pages/testimonials/Testimonials.jsx
@@ -1,12 +1,21 @@
 import './Testimonials.css'
+import useBackground from "../../../../workout-flow-webapp/src/hooks/useBackground.js";
+import ReviewBlock from "../../components/ui/reviewBlock/ReviewBlock.jsx";
 
-function Testimonials() {
+
+function Testimonials(){
+
+    useBackground("testimonials-background")
 
     return (
         <>
-
+            <div className="intro-text-testimonials">
+                <h1>What users are saying</h1>
+                <p>Still not convinced? See what our users love about Pumpd:</p>
+            </div>
+            <ReviewBlock></ReviewBlock>
         </>
-    )
-}
+    );
+};
 
-export default Testimonials
+export default Testimonials;
