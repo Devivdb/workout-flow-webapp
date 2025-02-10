9bc5e4ae3d2692f79664dbac522996eb08b26c08
Devivdb <devivdb@gmail.com>
2025-02-10 13:44:15 +0100

    Add base structure and CSS styling for the features page

--- src/pages/features/Features.jsx
+++ src/pages/features/Features.jsx
@@ -1,12 +1,56 @@
 import './Features.css'
+import useBackground from "../../../../workout-flow-webapp/src/hooks/useBackground.js";
+import featureOne from "../../../../workout-flow-webapp/src/assets/pictures/featureOne.png"
+import featureTwo from "../../../../workout-flow-webapp/src/assets/pictures/featureTwo.png"
+import {useState} from "react";
 
 function Features() {
 
+    useBackground("features-background")
+    const [selectedImage, setSelectedImage] = useState(featureOne)
+
+    useBackground("features-background")
+
+    const images = {
+        feature1: featureOne,
+        feature2: featureTwo,
+    }
+
+    const handleClick = (feature) => {
+        setSelectedImage(images[feature]);
+    }
+
     return (
         <>
+            <div className="outer-container-wrapper-features">
+                <div className="inner-container-wrapper-features">
+                    <h1>Features</h1>
+                    <p className="feature-intro-text">Explore the features that make Pumpd unique and powerful.</p>
+                    <div className="features-wrapper">
+                        <div className="feature-item"
+                             onClick={() => handleClick("feature1")}
+                        >
+                            <p>Build personalized routines with adjustable exercises, sets, reps, and rest times.</p>
+                        </div>
+                        <div className="feature-item"
+                             onClick={() => handleClick("feature2")}
+                        >
+                            <p>Build personalized routines with adjustable exercises, sets, reps, and rest times.</p>
+                        </div>
+                        <div className="feature-item"
+                             onClick={() => handleClick("feature1")}
+                        >
+                            <p>Build personalized routines with adjustable exercises, sets, reps, and rest times.</p>
+                        </div>
+                    </div>
+                </div>
+                <span className="image-wrapper">
+                    <img src={selectedImage} alt="Features"/>
+                </span>
+            </div>
 
         </>
-    )
-}
+    );
+};
 
-export default Features
+export default Features;
