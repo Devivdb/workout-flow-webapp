9d0f7aa95c75b6546f7e2b2e747f73103077badf
Devivdb <devivdb@gmail.com>
2025-02-10 12:57:51 +0100

    Router and route implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
 import './App.css'
+import {Routes, Route} from "react-router-dom";
 import Home from "./pages/home/Home.jsx";
+import Features from "./pages/features/Features.jsx";
+import Testimonials from "./pages/testimonials/Testimonials.jsx";
+import Highlights from "./pages/highlights/Highlights.jsx";
+import Contact from "./pages/contact/Contact.jsx";
+import Faq from "./pages/faq/Faq.jsx";
+import LogIn from "./pages/logIn/LogIn.jsx";
+import SignUp from "./pages/signUp/SignUp.jsx";
+import WorkoutBuilder from "./pages/workoutBuilder/WorkoutBuilder.jsx";
+import Search from "./pages/search/Search.jsx";
 
 function App() {
 
-  return (
-    <>
-      <Home/>
-    </>
-  )
+
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/features" element={<Features/>}/>
+                <Route path="/testimonials" element={<Testimonials/>}/>
+                <Route path="/highlights" element={<Highlights/>}/>
+                <Route path="/contact" element={<Contact/>}/>
+                <Route path="/faq" element={<Faq/>}/>
+                <Route path="/log-in" element={<LogIn/>}/>
+                <Route path="/sign-Up" element={<SignUp/>}/>
+                <Route path="/workout-builder" element={<WorkoutBuilder/>}/>
+                <Route path="/search" element={<Search/>}/>
+            </Routes>
+        </>
+    )
 }
 
 export default App
