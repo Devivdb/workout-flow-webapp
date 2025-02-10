74fc158dcd974e8e040f55a1b92e0eb759bf34cb
Devivdb <devivdb@gmail.com>
2025-02-10 14:25:29 +0100

    Add base structure and CSS styling for the highlights page

--- src/pages/features/Features.jsx
+++ src/pages/features/Features.jsx
@@ -30,17 +30,29 @@ function Features() {
                         <div className="feature-item"
                              onClick={() => handleClick("feature1")}
                         >
-                            <p>Build personalized routines with adjustable exercises, sets, reps, and rest times.</p>
+                            <h3>Export Workouts to PDF, CSV, and JSON</h3>
+                            <p>
+                                Easily save and share your workouts by exporting them in multiple formats. Download your
+                                routines as PDF for printing, CSV for spreadsheets, or JSON for data management.
+                            </p>
                         </div>
                         <div className="feature-item"
                              onClick={() => handleClick("feature2")}
                         >
-                            <p>Build personalized routines with adjustable exercises, sets, reps, and rest times.</p>
+                            <h3>Progress Tracking & Workout History</h3>
+                            <p>
+                                Keep track of your completed workouts and monitor your progress over time. Stay
+                                motivated with detailed workout history and performance insights.
+                            </p>
                         </div>
                         <div className="feature-item"
                              onClick={() => handleClick("feature1")}
                         >
-                            <p>Build personalized routines with adjustable exercises, sets, reps, and rest times.</p>
+                            <h3>Interactive Exercise Library</h3>
+                            <p>
+                                Browse a wide range of exercises with detailed instructions and demo videos. Find the
+                                perfect exercises to match your workout plan.
+                            </p>
                         </div>
                     </div>
                 </div>
--- src/pages/highlights/Highlights.jsx
+++ src/pages/highlights/Highlights.jsx
@@ -1,12 +1,47 @@
 import './Highlights.css'
+import useBackground from "../../../../workout-flow-webapp/src/hooks/useBackground.js";
 
 function Highlights() {
 
+    useBackground("highlights-background")
+
     return (
         <>
-
+            <div className="outer-container-wrapper-highlights">
+                <h1>Highlights</h1>
+                <div className="inner-container-wrapper-highlights">
+                    <div className="highlight">
+                        <h2>Custom Workout Creation & Management</h2>
+                        <p>
+                            Easily create and customize workout routines to fit your fitness goals. Add exercises, set
+                            repetitions, and organize workout schedules—all in one place.
+                        </p>
+                    </div>
+                    <div className="highlight">
+                        <h2>Custom Workout Creation & Management</h2>
+                        <p>
+                            Easily create and customize workout routines to fit your fitness goals. Add exercises, set
+                            repetitions, and organize workout schedules—all in one place.
+                        </p>
+                    </div>
+                    <div className="highlight">
+                        <h2>Custom Workout Creation & Management</h2>
+                        <p>
+                            Easily create and customize workout routines to fit your fitness goals. Add exercises, set
+                            repetitions, and organize workout schedules—all in one place.
+                        </p>
+                    </div>
+                    <div className="highlight">
+                        <h2>Custom Workout Creation & Management</h2>
+                        <p>
+                            Easily create and customize workout routines to fit your fitness goals. Add exercises, set
+                            repetitions, and organize workout schedules—all in one place.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </>
-    )
+    );
 }
 
-export default Highlights
+export default Highlights;
