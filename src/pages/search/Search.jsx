38323a7f9905cc79243f3ee4de0572a4cf9fd469
Devivdb <devivdb@gmail.com>
2025-04-27 08:18:26 +0200

    Semi working search page

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,20 +1,59 @@
 import './Search.css'
 import FiltersExercises from "../../components/filterExercises/FiltersExercises.jsx";
 import FilteredExercisesList from "../../components/ExerciseSelectionPanel/FilteredExercisesList.jsx";
-import {useContext} from "react";
-import {FiltersContext} from "../../context/filtersContext/FiltersContext.jsx";
+import { useContext, useEffect, useCallback } from "react";
+import { FiltersContext } from "../../context/filtersContext/FiltersContext.jsx";
+import fetchExercisesByEquipment from "../../services/api/fetchExercises.js";
 
 function Search() {
-
-    // TODO Make a new state for Search to be able to select the filters and see the result of the filters
-    //TODO With new state make sure the filters have a default value
-
     /// Context
     const {
+        selectedPrimaryMuscles,
+        activeFilters,
+        setExercises,
         filteredExercises,
+        setFilteredExercises,
         selectedExercises,
         handleExerciseSelection
-    } = useContext(FiltersContext)
+    } = useContext(FiltersContext);
+
+    // Memoize the filtering function
+    const filterExercises = useCallback(() => {
+        if (activeFilters.exercises && activeFilters.exercises.length > 0) {
+            const filtered = activeFilters.exercises.filter(exercise =>
+                (activeFilters.selectedLevel === "all" || exercise.level === activeFilters.selectedLevel) &&
+                (activeFilters.selectedEquipment === "all" || exercise.equipment === activeFilters.selectedEquipment) &&
+                (activeFilters.selectedForce === "all" || exercise.force === activeFilters.selectedForce) &&
+                (activeFilters.selectedMechanic === "all" || exercise.mechanic === activeFilters.selectedMechanic)
+            );
+            setFilteredExercises(filtered);
+        } else {
+            // Clear filtered exercises when there are no exercises in activeFilters
+            // This handles the reset filters case
+            setFilteredExercises([]);
+        }
+    }, [activeFilters, setFilteredExercises]);
+
+    // API fetch when the selected muscles change or when filters are reset
+    useEffect(() => {
+        async function fetchData() {
+            if (selectedPrimaryMuscles) {
+                const data = await fetchExercisesByEquipment(selectedPrimaryMuscles);
+                setExercises(data);
+            } else {
+                // When primary muscles selection is reset, clear exercises
+                setExercises([]);
+            }
+        }
+
+        fetchData();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [selectedPrimaryMuscles]); // removed setExercises from dependencies
+
+    // Apply filtering when activeFilters changes
+    useEffect(() => {
+        filterExercises();
+    }, [filterExercises]);
 
     return (
         <>
